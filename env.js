b712222e99289d29edf7681449be909605865350
Sourabh Agarwal <sourabh.max01@gmail.com>
2021-04-09 20:49:22 -0400

    Updates

---
+++ env.js
@@ -0,0 +1,10 @@
+window.env = {
+  "FIREBASE_API_KEY": "AIzaSyABH3hHQOrmYDIAf1KmFUs2QOtMKiB2QdY",
+  "FIREBASE_AUTH": "sourabhresume-268de.firebaseapp.com",
+  "FIREBASE_DATABASE": "https://sourabhresume-268de-default-rtdb.firebaseio.com",
+  "FIREBASE_PROJECT_ID": "sourabhresume-268de",
+  "FIREBASE_STORAGE_BUCKET": "sourabhresume-268de.appspot.com",
+  "FIREBASE_MESSAGING": "1059240331756",
+  "FIREBASE_APP_ID": "1:1059240331756:web:903a25398ad8c0d80d98c9",
+  "FIREBASE_MEASUREMNET_ID": "G-NZVTNGW8GP"
+};
